4bc8db084af41193e5d828411212fe45e66aeb32
leobgs <leonardobagus093@gmail.com>
2021-10-04 15:20:49 +0700

    penambahan footer "welcome"

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -17,7 +17,7 @@ class Latihan1 extends CI_Controller
         $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
 
         $this->load->view('view-latihan', $data);
-        
+        echo "welcome";
     }
 }
 
